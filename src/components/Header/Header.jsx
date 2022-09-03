735803c28e30bfee47bd7c7e84859696600b868f
karan <karansinghtopno@gmail.com>
2022-09-04 05:07:26 +0530

    adding animation & some responsiveness to site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import './Header.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { useState } from "react";
+import { Link } from "react-scroll";
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="header">
-        <img src={logo} alt="logoimage"  className='logo'/>
-        <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why Us</li>
-            <li>Plans</li>
-            <li>Testimonials</li>
+      <img src={logo} alt="logoimage" className="logo" />
+      {menuOpen === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpen(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li onClick={() => setMenuOpen(false)}>Home</li>
+          <li onClick={() => setMenuOpen(false)}>Programs</li>
+          <li onClick={() => setMenuOpen(false)}>Why Us</li>
+          <li onClick={() => setMenuOpen(false)}>Plans</li>
+          <li onClick={() => setMenuOpen(false)}>Testimonials</li>
         </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,21 @@ import hero_image from "../assets/hero_image.png";
 import hero_image_back from "../assets/hero_image_back.png";
 import Heart from "../assets/heart.png";
 import Calories from "../assets/calories.png";
+import { motion } from "framer-motion";
 const Hero = () => {
+  const transition={type:'spring',duration:3}
+  const mobile= window.innerWidth<=768?true:false;
   return (
     <div className="hero">
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
         {/* {the beat add} */}
         <div className="the-best-ad">
-          <div></div>
+          <motion.div 
+          initial={{left:mobile?"145px":"238px"}}
+          whileInView={{left:'8px'}}
+          transition={{...transition,type:'tween'}}></motion.div>
           <span>The best fitness club int he town</span>
         </div>
         {/* hero heading */}
@@ -56,29 +63,41 @@ const Hero = () => {
       <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heart-rate">
+        <motion.div 
+        initial={{right:'-1rem'}}
+        whileInView={{right:'4rem'}}
+        transition={transition}
+        className="heart-rate">
           <img src={Heart} alt="heart" />
           <span>Heart Rate</span>
           <span>116 bpm</span>
-        </div>
+        </motion.div>
 
         {/* hero Image */}
         <img src={hero_image} alt="heroImage" className="hero-image" />
-        <img
+        <motion.img
+        initial={{right:'11rem'}}
+        whileInView={{right:'20rem'}}
+        transition={transition}
           src={hero_image_back}
           alt="HeroImageBack"
           className="hero-image-back"
         />
 
         {/* calories */}
-        <div className="calories">
+        <motion.div 
+        initial={{right:'37rem'}}
+        whileInView={{right:'28rem'}}
+        transition={transition}
+
+        className="calories">
           <img src={Calories} alt="calories" />
           <div>
           <span>Calories Burned</span>
           <span>220 kcal</span>
           </div>
           
-        </div>
+        </motion.div>
       </div>
     </div>
   );
